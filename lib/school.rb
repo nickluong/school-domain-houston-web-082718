375925ecab60e5e8b745611981d4bb3bfcb8bde1
Nicholas  Luong <nckluong@gmail.com>
2018-08-29 21:47:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School 
+  
+  NEW = []
+  
+  def initialize(new)
+    new = NEW 
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if(roster[grade] == nil)
+    @roster[grade] = []
+    @roster[grade] << name
+    #create new grade and have a new array to hold student names 
+    else 
+    @roster[grade] << name  
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade].each do|student_names|
+      student_names
+    end 
+  end 
+    
+  def sort
+    @roster.each do |grade|
+      
+  end 
+  
+end 
+  
